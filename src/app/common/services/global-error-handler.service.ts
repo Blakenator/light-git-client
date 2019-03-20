af96c2e7cc9649b91bab78f0e29c018977aed0de
Blakenator <blakestacks@rocketmail.com>
2019-03-19 20:04:22 -0500

    Display angular errors in error service

--- src/app/common/services/global-error-handler.service.ts
+++ src/app/common/services/global-error-handler.service.ts
@@ -1,21 +1,24 @@
-import {ErrorHandler, Injectable} from '@angular/core';
-import {ElectronService} from "./electron.service";
-import {Channels} from "../../../../shared/Channels";
+import {ErrorHandler, Injectable, Injector} from '@angular/core';
+import {ElectronService} from './electron.service';
+import {Channels} from '../../../../shared/Channels';
+import {ErrorService} from './error.service';
+import {ErrorModel} from '../../../../shared/common/error.model';
 
 @Injectable()
 export class GlobalErrorHandlerService implements ErrorHandler {
   error: any = {stack: ''};
 
-  constructor() {
+  constructor(private injector: Injector) {
   }
 
   handleError(error: any): void {
     console.error(error);
     const errorText = error.stack || JSON.stringify(error);
     new ElectronService().rpc(Channels.LOG, [errorText]);
-    // if ((errorText) != (this.error.stack || JSON.stringify(this.error))) {
-    //   alert(errorText);
-    // }
     this.error = error;
+    const errorService = this.injector.get(ErrorService);
+    if (errorService && !error.stack.match(/expressionchangedafterithasbeencheckederror/i)) {
+      errorService.receiveError(new ErrorModel('angularError', 'performing an unknown operation', error.stack));
+    }
   }
 }
